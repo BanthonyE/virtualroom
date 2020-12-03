0632558941bbc3810a93332474098d13b26971c0
BanthonyE <anthony220e@gmail.com>
2020-12-02 22:28:13 -0500

    se removió composer lock de gitignore para el deploy en heroku

--- application/views/layout/footer_public.php
+++ application/views/layout/footer_public.php
@@ -78,9 +78,7 @@
 </div>
 <!-- /.modal -->
 
-
-
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="<?php base_url() ?>assets/js/jquery-3.4.1.min.js"></script>
 <script src="<?php base_url() ?>assets/bootstrap/js/bootstrap.js"></script>
 <script src="<?php base_url() ?>assets/js/script.js"></script>
 
