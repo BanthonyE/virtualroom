57bd162e754dae7e5ca3f0a139b35b2864fb24ce
BanthonyE <anthony220e@gmail.com>
2020-12-05 04:02:49 -0500

    se actualizó el proyecto

--- application/views/login_admin.php
+++ application/views/login_admin.php
@@ -38,7 +38,7 @@
             	   </div>
             	</div>
             	<input type="submit" class="btn" value="Entrar">
-				<button onclick="window.location.href='Inicio'">Continue</button>
+				<button class="btn" onclick="window.location.href='Inicio'">Continue</button>
             </form>
         </div>
 
